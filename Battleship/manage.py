b8c0ae461c32744724f72985edad2ee67d9ff685
amardaus <amardaus@gmail.com>
2021-12-07 16:27:29 +0100

    Rename

--- Battleship/manage.py
+++ Battleship/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Batlleships.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Battleship.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:

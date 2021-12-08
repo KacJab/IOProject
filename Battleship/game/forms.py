4bd2fc5cdb650771f56943384a79e1a22400fdfa
amardaus <amardaus@gmail.com>
2021-12-08 18:21:37 +0100

    Skladanie projektu czesc 3

--- Battleship/game/forms.py
+++ Battleship/game/forms.py
@@ -3,7 +3,16 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 from django.contrib.auth.models import User
 
+
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+    def __init__(self, *args, **kwargs):
+        super(CreateUserForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['email'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
